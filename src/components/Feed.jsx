cd2d00ae7ca2b67793bf5e874ba4d8f0909a453d
leo-st <leoivas2103@gmail.com>
2024-12-16 23:04:16 +0100

    adding static cards that will later be fetched from api (intermediate step from dummy data)

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,27 @@
-function Feed(){
-    return <h1>Feed Main</h1>
+
+import './Feed.css'
+import SportCard from "./SportCard";
+function Feed() {
+  return (
+    <div>
+      <h1>Sports</h1>
+      <ul>
+        <li>
+        <SportCard
+                image="/images/football.png"
+                description="Football"
+            />
+        </li>
+        <li>
+        <SportCard
+                image="/images/basketball.png"
+                description="Basketball"
+            />
+        </li>
+      </ul>
+      
+    </div>
+  );
 }
 
 export default Feed;
