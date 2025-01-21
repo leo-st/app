500b28e7f00001a75eec6fa7c3802492add5ab21
leo-st <leoivas2103@gmail.com>
2025-01-21 22:13:33 +0100

    updated feed list with additional main categories

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./Feed.css";
 import SportCard from "./SportCard";
 import ChatBox from "./ChatBox";
-function Feed({friendChat}) {
+function Feed({ friendChat }) {
   console.log("feed rendered");
   console.log(friendChat);
-  const [showOverlay, setShowOverlay] = useState(friendChat ? true : false );
+  const [showOverlay, setShowOverlay] = useState(friendChat ? true : false);
 
-  function hideChatBox(){
+  function hideChatBox() {
     setShowOverlay(false);
   }
 
@@ -15,20 +15,36 @@ function Feed({friendChat}) {
     setShowOverlay(friendChat ? true : false);
   }, [friendChat]); // Run whenever `friendChat` changes
 
+  const sports = [
+    { description: "Football", url: "/images/football.png" },
+    { description: "Basketball", url: "/images/basketball.png" },
+    { description: "Badminton", url: "/images/badminton.png" },
+    { description: "Padel", url: "/images/padel.png" },
+    { description: "Table Tenis", url: "/images/table_tenis.png" },
+    { description: "Tenis", url: "/images/tenis.png" },
+    { description: "Voleyball", url: "/images/voleyball.png" },
+  ];
+
   return (
     <div className="feed">
       <h1>Sports</h1>
-      <ul>
-        <li>
-          <SportCard image="/images/football.png" description="Football" />
-        </li>
-        <li>
-          <SportCard image="/images/basketball.png" description="Basketball" />
-        </li>
-      </ul>
+      <div>
+        <div className="list-categories-container">
+          <button className="categories-button">Open all categories</button>
+        </div>
+        <div className="sport-card-container">
+          <ul>
+            {sports.map((sport) => (
+              <li>
+                <SportCard image={sport.url} description={sport.description} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
 
       {showOverlay && (
-        <ChatBox onShowOverlay={hideChatBox} friend={friendChat}/>
+        <ChatBox onShowOverlay={hideChatBox} friend={friendChat} />
       )}
 
       {!showOverlay && (
